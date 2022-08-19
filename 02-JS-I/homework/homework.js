18660ec04c3adf788fb2e2d5f11e4a753c493518
oskarp88 <oskyburgos81@gmail.com>
2022-08-19 12:34:28 -0500

    respuestas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -209,7 +209,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return console.log("hola " + nombre);
+   console.log("hola " + nombre);
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
